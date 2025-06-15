b265b174d1038eabe68113837b67e2758c641e66
VP <vopenderchaudhary@gmail.com>
2025-06-15 15:40:10 +0000

    coversionoperation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************* OPERATION ******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "helloe"
+let str2 = "vopender"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2 );
+// console.log(1 + "2");
+// console.log(1 + 2 + "2" );
+
+// console.log(true);
+ 
+let num1, num2, bum3
+
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
