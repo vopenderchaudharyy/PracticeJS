41b5d6461ab4562dccf433e8b3514e060914189d
VP <vopenderchaudhary@gmail.com>
2025-06-16 12:49:17 +0000

    comparison js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,29 @@
+// console.log(3>1)
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//node 01_basics/04_comparison.js
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
